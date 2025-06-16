9b20139d65e7dcc332ecbea758df9cb1150045d0
skovacs4 <kovacsstefy46@gmail.com>
2025-06-16 17:38:08 +0300

    Added new api for kronburg.

---
+++ src/routes/api/sendEmailKronburg/+server.js
@@ -0,0 +1,85 @@
+import nodemailer from "nodemailer";
+import { EMAIL_KRONBURG, EMAIL_PASSWORD_KRONBURG } from "$env/static/private";
+
+// Create transporter with authentication details
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: EMAIL_KRONBURG,
+        pass: EMAIL_PASSWORD_KRONBURG,
+    },
+});
+
+// Verify transporter setup (optional)
+transporter.verify(function (error, success) {
+    if (error) {
+        console.error("Error verifying transporter:", error);
+    } else {
+        console.log("Email transporter ready");
+    }
+});
+
+/**
+ * API Route - Handle POST requests to send emails
+ */
+export async function POST({ request }) {
+    const requestBody = await request.json();
+
+    const { to, subject, text, html, fromDisplayName } = requestBody;
+
+    // Set sender dynamically based on request parameters
+    var sender = "";
+
+    if (fromDisplayName && fromDisplayName.trim() !== "") {
+        sender = `"${fromDisplayName}" <${EMAIL_KRONBURG}>`;
+    } else {
+        // Fallback display name if fromDisplayName is not provided or empty
+        sender = `"Contact" <${EMAIL_KRONBURG}>`;
+    }
+
+    try {
+        // Send email using the configured transporter with dynamic sender
+        await transporter.sendMail({ from: sender, to, subject, text, html });
+
+        // Set CORS headers to allow all origins
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+        };
+
+        // Return success response
+        return new Response(JSON.stringify({ message: 'Email sent!' }), { status: 200, headers });
+    } catch (error) {
+        console.error("Error sending email:", error);
+
+        // Return error response
+        return new Response(JSON.stringify({ error: 'Failed to send email' }), { status: 500 });
+    }
+}
+
+export async function OPTIONS() {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    return new Response(null, { status: 204, headers });
+}
+
+/**
+ * API Route - Handle GET requests
+ */
+export async function GET() {
+    // Set CORS headers to allow all origins
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    // Return response for GET request
+    return new Response(JSON.stringify({ message: 'Hello from GET!' }), { status: 200, headers });
+}
